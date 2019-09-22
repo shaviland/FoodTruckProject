2e4c7d3b854bf4cbb6ff944245e65238a854bc65
Samantha Haviland <shaviland@live.com>
2019-09-22 14:06:07 -0600

    another commit

--- src/com/skilldistillery/foodtrucks/FoodTruck.java
+++ src/com/skilldistillery/foodtrucks/FoodTruck.java
@@ -1,26 +1,25 @@
 package com.skilldistillery.foodtrucks;
 
+import java.util.Scanner;
+
 public class FoodTruck {
-	
+
 	private String name;
 	private String type;
-	private int truckID;
+	private static int truckID = 1300;
 	private int truckRating;
-	
-	
+
 	public FoodTruck() {
 		super();
 	}
-
 
 	public FoodTruck(String name, String type, int truckID, int truckRating) {
 		super();
 		this.name = name;
 		this.type = type;
-		this.truckID = truckID;
+		FoodTruck.truckID = truckID;
 		this.truckRating = truckRating;
 	}
-
 
 	@Override
 	public String toString() {
@@ -28,48 +27,113 @@ public class FoodTruck {
 				truckRating);
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getType() {
 		return type;
 	}
-
 
 	public void setType(String type) {
 		this.type = type;
 	}
 
-
 	public int getTruckID() {
 		return truckID;
 	}
 
-
 	public void setTruckID(int truckID) {
-		this.truckID = truckID;
+		truckID++;
+		FoodTruck.truckID = truckID;
 	}
-
 
 	public int getTruckRating() {
 		return truckRating;
 	}
 
-
 	public void setTruckRating(int truckRating) {
 		this.truckRating = truckRating;
 	}
-	
-	
-	
-	
 
+	public FoodTruck[] truckInput(Scanner kb) {
+		// eclipse isn't allowing the method to be non-static.
+
+		System.out.println("Welcome to Food Truck ratings!");
+		FoodTruck[] truckGroup = new FoodTruck[5];
+		for (int i = 0; i < truckGroup.length; i++) {
+			FoodTruck ft = new FoodTruck();
+
+			System.out.println("Please input truck name or \"quit\" to exit:");
+			String name = kb.nextLine();
+			kb.nextLine();
+			if (name.equalsIgnoreCase("quit")) {
+				break;
+			} else {
+				ft.setName(name);
+				ft.setTruckID(truckID);
+				System.out.println("Please input food type:");
+				String type = kb.nextLine();
+				kb.nextLine();
+				ft.setType(type);
+				System.out.println("Please input rating (1-10)");
+				int truckRating = kb.nextInt();
+				kb.nextLine();
+				ft.setTruckRating(truckRating);
+				truckGroup[i] = ft;
+				truckGroup[i].setTruckID(truckID);
+
+			}
+
+		}
+		resultsMenu(kb, truckGroup);
+		return truckGroup;
+	}
+
+	public void resultsMenu(Scanner kb, FoodTruck[] truckGroup) {
+		boolean quit = false;
+		do {
+			System.out.println("Please select an option:");
+			System.out.println("1. List all existing food trucks.");
+			System.out.println("2. See the average rating of food trucks.");
+			System.out.println("3. Display the highest-rated food truck.");
+			System.out.println("4. Quit");
+			int selection = kb.nextInt();
+
+			if (selection > 4 || selection < 1) {
+				System.err.println("That is not a valid selection.");
+			} else if (selection == 1) {
+				for (int i = 0; i < truckGroup.length; i++) {
+					if (truckGroup[i] == null) {
+						continue;
+					} else {
+						System.out.println("Truck " + (i + 1) + ": " + truckGroup[i]);
+					}
+				}
+			} else if (selection == 2) {
+				int j = 0;
+				int newRate = 0;
+				for (int i = 0; i < truckGroup.length; i++) {
+					if (truckGroup[i] == null) {
+						break;
+					}
+					int rate = truckGroup[i].getTruckRating();
+					System.out.println(rate);
+					newRate += rate;
+					System.out.println("new rate " + newRate);
+					j = i;
+					System.out.println("# " + j);
+
+				}
+				System.out.println("The average food truck rating is: " + (newRate / j));
+			}else if(selection == 3) {
+				
+			}
+
+		} while (quit != true);
+	}
 }
--- src/com/skilldistillery/foodtrucks/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/FoodTruckApp.java
@@ -6,43 +6,14 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
+		FoodTruck foodTruck = new FoodTruck();
 		
-		menu(kb);
 		
+		foodTruck.truckInput(kb);
 
+
+		
 	}
 
-	public static void menu(Scanner kb) {
 		
-		System.out.println("Welcome to Food Truck ratings!");
-		FoodTruck[] truckGroup = new FoodTruck[5];
-		for (int i = 0; i < truckGroup.length; i++) {
-			FoodTruck ft = new FoodTruck();
-
-			System.out.println("Please input truck name or \"quit\" to exit:");
-			String name = kb.nextLine();
-			kb.hasNextLine();
-			if (name.equalsIgnoreCase("quit")) {
-				break;
-			} else {
-				ft.setName(name);
-				System.out.println("Please input food type:");
-				String type = kb.nextLine();
-				kb.nextLine();
-				ft.setType(type);
-				System.out.println("Please input rating (1-10)");
-				int truckRating = kb.nextInt();
-				kb.nextLine();
-				ft.setTruckRating(truckRating);
-//				System.out.println(ft);
-				truckGroup[i] = ft;
-			}
-//			System.out.println(truckGroup[i]);
-		}
-		
-			
-			
-
-	}
-
 }
